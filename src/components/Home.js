2a0bea206d2f08abc742ba20148de6ec4e984f0b
Ricky Tanudjaja <rickytanudjaja95@gmail.com>
2021-05-07 10:32:30 +0700

    add name to title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import UserContext from "../context/UserContext"
 import Image from 'react-bootstrap/Image'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
@@ -6,10 +7,12 @@ import Container from 'react-bootstrap/Container'
 import Login from './Login';
 
 const Home = (props) => {
+  const { user } = useContext(UserContext);
+
   return (
     <Jumbotron className="h-100 d-flex align-items-center justify-content-center" >
       <Container className="w-100 d-flex flex-column align-items-center">
-        <h1>My E-Wallet</h1>
+        <h1>{user.name !== "" ? user.name + "'s" : "My"} E-Wallet</h1>
         <Image src="../../wallet.png" fluid roundedCircle />
         <Login {...props} />
       </Container>
