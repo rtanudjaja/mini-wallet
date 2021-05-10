286757c53fb27a270f0bd4a1ccc9fa5bf689ca0f
Ricky Tanudjaja <rickytanudjaja95@gmail.com>
2021-05-10 13:49:30 +0700

    update mock api link

--- src/utils/http-mock.js
+++ src/utils/http-mock.js
@@ -2,13 +2,13 @@ import axios from "axios"
 
 const http = (token) => {
   return token ? axios.create({
-    baseURL: "https://pure-hamlet-76363.herokuapp.com/",
+    baseURL: "https://pure-hamlet-76363.herokuapp.com/api/v1",
     headers: {
       "Content-type": "application/json",
       "Authorization": "Token " + token,
     }
   }) : axios.create({
-    baseURL: "https://pure-hamlet-76363.herokuapp.com/",
+    baseURL: "https://pure-hamlet-76363.herokuapp.com/api/v1/",
     headers: {
       "Content-type": "application/json",
     }
